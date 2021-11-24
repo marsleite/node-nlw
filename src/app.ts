8924cea7d956be1fc6dac3d1b76d379fbcc28ee1
Marcelo <marsleite@gmail.com>
2021-11-24 09:15:57 -0300

    feita a rota de auth e callback do sigin do github

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
-import "dotenv/config"
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/github', (req, res) => {
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+app.get('/github', (_req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${ process.env.GITHUB_CLIENT_ID }`);
 });
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query;
+
+  return res.json({ code });
+});
+
 
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
